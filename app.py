8bc883b80aa3885dceed64d24b7f3df9fee45960
skazal15 <khan.said86@gmail.com>
2022-09-08 05:52:57 +0700

    wedding

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from odm.data import WishPost,WishGet
 root_dir = os.path.abspath(os.path.dirname(__file__))
 template_dir = os.path.join(root_dir,'templates')
 static_dir = os.path.join(root_dir,'static')
-config = {'CACHE_TYPE':'SimpleCache'}
+#config = {'CACHE_TYPE':'SimpleCache'}
 male="Muhammad Sai'd Fadhiil,S.T."
 female = "Dyah Puspita Sari,S.P.,M.P"
 couple = "Said & Dyah"
@@ -25,11 +25,11 @@ norekening1 = "0700009709259"
 rekening = "Dyah Puspita Sari"
 norekening = "025601081686503"
 app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
-app.config.from_mapping(config)
-cache = Cache(app)
+#app.config.from_mapping(config)
+#cache = Cache(app)
 
 @app.route('/',methods=['POST','GET'])
-@cache.cached(timeout=1)
+#@cache.cached(timeout=1)
 def home():
     receive = request.args.get('kepada')
     return render_template('index444b.html',datadate=datadate,receive=receive,male=male,female=female,couple=couple,date=date,pfemale=pfemale,pmale=pmale,day=day,atime=atime,start=start,end=end,nameplace=nameplace,address=address,rekening1=rekening1,norekening1=norekening1,rekening=rekening,norekening=norekening)
