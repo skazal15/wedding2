433ca888a237b1349cfd700f828786b14e729d2c
skazal15 <khan.said86@gmail.com>
2022-09-24 13:48:09 +0700

    update

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ male="Muhammad Sai'd Fadhiil,S.T."
 female = "Dyah Puspita Sari,S.P.,M.P"
 couple = "Said & Dyah"
 date = "16 Oktober 2022"
-pfemale = "Putri pertama dari Bapak Sugiyono, S.Pd., M.Pd & Ibu Dewi Variyanti, S.Pd"
-pmale = "Putra pertama dari Bapak Andi Setiawan, S.E. & Ibu Aini Almadany, S.E."
+pfemale = "Bapak Sugiyono, S.Pd., M.Pd & Ibu Dewi Variyanti, S.Pd"
+pmale = "Bapak Andi Setiawan, S.E. & Ibu Aini Almadany, S.E."
 day = "Minggu"
 atime = "08:00 WIB"
 start = "10:00"
@@ -23,15 +23,25 @@ norekening1 = "0700009709259"
 rekening = "Dyah Puspita Sari"
 norekening = "025601081686503"
 datadate = "1665854396"
+acarakami = "pernikahan kami"
+acaraortu = "pernikahan putra putri kami"
+redaxkami = "Kami yang berbahagia"
+redaxortu = "Hormat Kami yang Menanti"
 app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 #app.config.from_mapping(config)
 #cache = Cache(app)
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/our',methods=['POST','GET'])
+#@cache.cached(timeout=1)
+def our():
+    receive = request.args.get('kepada')
+    return render_template('index444b.html',datadate=datadate,receive=receive,male=male,female=female,couple=couple,date=date,pfemale=pfemale,pmale=pmale,day=day,atime=atime,start=start,end=end,nameplace=nameplace,rekening1=rekening1,norekening1=norekening1,rekening=rekening,norekening=norekening,acara=acarakami,redax=redaxkami, view='kami')
+
+@app.route('/ortu',methods=['POST','GET'])
 #@cache.cached(timeout=1)
 def home():
     receive = request.args.get('kepada')
-    return render_template('index444b.html',datadate=datadate,receive=receive,male=male,female=female,couple=couple,date=date,pfemale=pfemale,pmale=pmale,day=day,atime=atime,start=start,end=end,nameplace=nameplace,rekening1=rekening1,norekening1=norekening1,rekening=rekening,norekening=norekening)
+    return render_template('index444b.html',datadate=datadate,receive=receive,male=male,female=female,couple=couple,date=date,pfemale=pfemale,pmale=pmale,day=day,atime=atime,start=start,end=end,nameplace=nameplace,rekening1=rekening1,norekening1=norekening1,rekening=rekening,norekening=norekening,acara=acaraortu,redax=redaxortu, view='ortu')
 
 @app.route('/post',methods=['POST','GET'])
 def post():
--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from waitress import serve
 from app import app
 
 if __name__ == "__main__":
-    serve(app,port=12514,threads=20)
+    serve(app,port=12514,threads=100)
 
 #if __name__ == "__main__":
 #    app.run()
